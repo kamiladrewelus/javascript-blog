5eb939f7469114af3e7f63abd23cacfe59d6821c
Kamila Drewelus <kamila.drewelus@gmail.com>
2020-01-06 16:19:35 +0100

    add class active

--- js/script.js
+++ js/script.js
@@ -5,16 +5,21 @@ document.getElementById('test-button').addEventListener('click', function(){
 //kliknięciu guzika funkcja querySelectorAll znalazła wszystkie elementy pasujące do selektora .titles a czyli 10 arty.
 
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
 
-  /*[done] remove class 'active' from all article links  */
+  /*[DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
-  /* remove class 'active' from all articles */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+
+  clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post.active');
 
   for(let activeArticle of activeArticles){
