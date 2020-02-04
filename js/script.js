f35db40ab80f4b05c41e90552059cc4f641871a4
Kamila Drewelus <kamila.drewelus@gmail.com>
2020-02-04 23:41:08 +0100

    final_Generowanie list tytulow

--- js/script.js
+++ js/script.js
@@ -3,11 +3,37 @@
 //  console.log('links:', links);
 //});
 //kliknięciu guzika funkcja querySelectorAll znalazła wszystkie elementy pasujące do selektora .titles a czyli 10 arty.
+const generateTitleLinks = function(){
+  /* usuń zawartość listy linków w lewej kolumnie*/
+  const titleList = document.querySelector('.titles');
+  titleList.innerHTML ='';
+  /*console.log('co jest w liście',titleList)*/
+  /*następnie dla każdego artykułu:*/
+  const articles = document.querySelectorAll('.post');
+  let html = '';
+  for(let article of articles){
+    /*odczytaj jego id i zapisz je do stałej,*/
+    const articleId = article.getAttribute('id');
+    /*znajdź element z tytułem i zapisz jego zawartość do stałej,*/
+    const articleTitle = article.querySelector('.post-title').textContent;
+    /*na podstawie tych informacji stwórz kod HTML linka i zapisz go do stałej,*/
+    const linkHTML = '<li><a href="#'+ articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    console.log('co to articleTitle',articleTitle);
+    html = html + linkHTML;
+    console.log('co to articleId',articleId);
+  }
+  /*wstaw stworzony kod HTML do listy linków w lewej kolumnie. */
+
+  titleList.innerHTML = html;
+}
+
+generateTitleLinks();
 
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!');
+  console.log('Link was clicked!yes');
 
   /*[DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -40,6 +66,7 @@ const titleClickHandler = function(event){
 }
 
 const links = document.querySelectorAll('.titles a');
+console.log('co zawiera stala, links');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
